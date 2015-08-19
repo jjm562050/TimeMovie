36cee22c4b37a4f1b8cfb64020e0d1909bf22f48
MAC22 <mac22@mac22deMacBook-Pro.local>
2015-08-19 11:23:37 +0800

    Initial Commit

---
+++ TimeMovie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeMovie
+//
+//  Created by MAC22 on 15/8/19.
+//  Copyright (c) 2015年 huiwenjiaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
